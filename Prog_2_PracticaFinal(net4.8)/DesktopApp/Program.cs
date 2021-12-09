a4aa214ceb9aa02264c3917a4fb442a7d7742a07
Edwin Roman <romanseberino@gmail.com>
2021-12-09 14:57:33 -0400

    Organizacion del orden de ejecucion
    Aquí he reorganizado el orden de ejecución de los formularios y uno que otros cambios realizados automaticamente por VS.

--- Prog_2_PracticaFinal(net4.8)/DesktopApp/Program.cs
+++ Prog_2_PracticaFinal(net4.8)/DesktopApp/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using DesktopApp.SplashScreen;
 
 namespace DesktopApp
 {
@@ -17,7 +16,8 @@ namespace DesktopApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MenuPrincipalMDI());
+            Application.Run(new SplashScreen.SplashScreen());
+            Application.Run(new LoginForm.LoginForm());
         }
     }
 }
