cf7efe8a71c93a0be660ef5b980fecb08adccdfd
Edwin Roman <romanseberino@gmail.com>
2021-12-09 12:07:03 -0400

    Aplicando menu principal con los distintos estilos y funcionalidades basicas que demanda la logica de negocio

---
+++ Prog_2_PracticaFinal(net4.8)/DesktopApp/MenuPrincipalMDI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp
+{
+    public partial class MenuPrincipalMDI : Form
+    {
+        public MenuPrincipalMDI()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime _dateSistema;
+
+        private void MenuPrincipalMDI_Load(object sender, EventArgs e)
+        {
+            //Haz esto en el evento Load de tu formulario MDI Para cambiar su Background
+
+            MdiClient oMDI;
+
+            //recorremos todos los controles hijos del formulario
+            foreach (Control ctl in this.Controls)
+            {
+                try
+                {
+                    // Intentamos castear el objeto MdiClient
+                    oMDI = (MdiClient)ctl;
+
+                    // Cuando sea casteado con éxito, podremos cambiar el color así
+                    oMDI.BackColor = Color.FromArgb(21,48,68);
+                }
+                catch (InvalidCastException exc)
+                {
+                    // No hacemos nada cuando el control no sea tupo MdiClient
+                }
+
+            }
+
+            this._dateSistema = DateTime.UtcNow.Date;
+            this.toolStripStatusFechaSistema.Text += this._dateSistema.ToString();
+
+        }
+    }
+}
