3f5a2795c375ad7e5f309c4d86d5f49eeb8b7fe4
Edwin Roman <romanseberino@gmail.com>
2021-12-09 13:38:59 -0400

    Codigo autogenerado para el diseno recien aplicado

---
+++ Prog_2_PracticaFinal(net4.8)/DesktopApp/MenuPrincipalMDI.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace DesktopApp
+{
+    partial class MenuPrincipalMDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStripGeneral = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grupoEntidadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tiposDeEntidadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.entidadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administradorDeVentanasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusFechaSistema = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStripGeneral.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStripGeneral
+            // 
+            this.menuStripGeneral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.menuStripGeneral.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.sistemaToolStripMenuItem,
+            this.administradorDeVentanasToolStripMenuItem});
+            this.menuStripGeneral.Location = new System.Drawing.Point(0, 0);
+            this.menuStripGeneral.MdiWindowListItem = this.administradorDeVentanasToolStripMenuItem;
+            this.menuStripGeneral.Name = "menuStripGeneral";
+            this.menuStripGeneral.Size = new System.Drawing.Size(1154, 24);
+            this.menuStripGeneral.TabIndex = 1;
+            this.menuStripGeneral.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grupoEntidadesToolStripMenuItem,
+            this.tiposDeEntidadesToolStripMenuItem,
+            this.entidadesToolStripMenuItem});
+            this.archivoToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.archivoToolStripMenuItem.Text = "Archivos... ▾";
+            // 
+            // grupoEntidadesToolStripMenuItem
+            // 
+            this.grupoEntidadesToolStripMenuItem.Name = "grupoEntidadesToolStripMenuItem";
+            this.grupoEntidadesToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.grupoEntidadesToolStripMenuItem.Text = "Grupo Entidades";
+            // 
+            // tiposDeEntidadesToolStripMenuItem
+            // 
+            this.tiposDeEntidadesToolStripMenuItem.Name = "tiposDeEntidadesToolStripMenuItem";
+            this.tiposDeEntidadesToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.tiposDeEntidadesToolStripMenuItem.Text = "Tipos de Entidades";
+            // 
+            // entidadesToolStripMenuItem
+            // 
+            this.entidadesToolStripMenuItem.Name = "entidadesToolStripMenuItem";
+            this.entidadesToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.entidadesToolStripMenuItem.Text = "Entidades";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem,
+            this.loginToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.sistemaToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.sistemaToolStripMenuItem.Text = "Sistema ... ▾";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // administradorDeVentanasToolStripMenuItem
+            // 
+            this.administradorDeVentanasToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.administradorDeVentanasToolStripMenuItem.Name = "administradorDeVentanasToolStripMenuItem";
+            this.administradorDeVentanasToolStripMenuItem.Size = new System.Drawing.Size(183, 20);
+            this.administradorDeVentanasToolStripMenuItem.Text = "Administrador de Ventanas... ▾";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusFechaSistema,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 551);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1154, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(209, 17);
+            this.toolStripStatusLabel1.Text = "Usuario Activo: Ricardo@hotmail.com";
+            // 
+            // toolStripStatusFechaSistema
+            // 
+            this.toolStripStatusFechaSistema.Name = "toolStripStatusFechaSistema";
+            this.toolStripStatusFechaSistema.Size = new System.Drawing.Size(807, 17);
+            this.toolStripStatusFechaSistema.Spring = true;
+            this.toolStripStatusFechaSistema.Text = "Fecha:  ";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(123, 17);
+            this.toolStripStatusLabel3.Text = "Tipo Usuario: Suplidor";
+            // 
+            // MenuPrincipalMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1154, 573);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStripGeneral);
+            this.ForeColor = System.Drawing.Color.White;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStripGeneral;
+            this.Name = "MenuPrincipalMDI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Principal";
+            this.Load += new System.EventHandler(this.MenuPrincipalMDI_Load);
+            this.menuStripGeneral.ResumeLayout(false);
+            this.menuStripGeneral.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripGeneral;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grupoEntidadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tiposDeEntidadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem entidadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem administradorDeVentanasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusFechaSistema;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
